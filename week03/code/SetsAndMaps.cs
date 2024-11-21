ddcfe2f58e2911c969e28a3d896e592f1a8ae695
Ashish Jain <ashishjain99@yahoo.com>
2024-11-21 00:49:05 -0700

    Added Custom FUnction

--- week03/code/SetsAndMaps.cs
+++ week03/code/SetsAndMaps.cs
@@ -91,11 +91,40 @@ public static class SetsAndMaps
     /// Reminder: You can access a letter by index in a string by 
     /// using the [] notation.
     /// </summary>
+    public static string OrderedNoSpace(string word)
+    {
+        string temp = "";
+        foreach(char c in word.ToLower())
+        {
+            if(c.ToString() == " ")
+            {
+                continue;
+            }
+            temp+=c;
+        }
+        char[] character = temp.ToCharArray();
+        for(int i = 0; i < character.Length - 1; i++)
+        {
+            for(int j = 0; j < character.Length - i - 1; j++)
+            {
+                if((int)character[j] > character[j + 1])
+                {
+                    char temporary = character[j];
+                    character[j] = character[j + 1];
+                    character[j + 1] = temporary;
+
+                }
+            }
+        }
+        string orderSorted = new string(character);
+        return orderSorted;
+    }
     public static bool IsAnagram(string word1, string word2)
     {
         // TODO Problem 3 - ADD YOUR CODE HERE
-        string sorted1 = new string(word1.ToLower().OrderBy(c => c).ToArray());
-        string sorted2 = new string(word2.ToLower().OrderBy(c => c).ToArray());
+        
+        string sorted1 = new string(OrderedNoSpace(word1));
+        string sorted2 = new string(OrderedNoSpace(word2));
         Dictionary<string, string> words = new Dictionary<string, string>() 
         {
             {"word1",sorted1},
